2ec89149c13a0382dc4c9d486e3ebed0ffe846ea
percy woeds <percywoeds@gmail.com>
2016-12-02 18:17:55 -0500

    vendor compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,10 @@ Mnygo::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
-  config.eager_load=true 
+  config.eager_load= true 
+
+  config.assets.compile = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
