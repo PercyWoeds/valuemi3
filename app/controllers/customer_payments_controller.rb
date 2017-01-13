2f512227d1fb2f038489cb0e7e72969c0915b32b
percy woeds <percywoeds@gmail.com>
2017-01-13 14:47:09 -0500

    abc

--- app/controllers/customer_payments_controller.rb
+++ app/controllers/customer_payments_controller.rb
@@ -130,7 +130,7 @@ class CustomerPaymentsController < ApplicationController
       row<< " " 
       row<< " "
       row<< @customerpayment.total.to_s 
-      $lcDeposito = @customerpayment.total.to_s 
+      $lcDeposito =sprintf("%.2f",@customerpayment.total.to_s) 
       table_content << row     
 
 
@@ -173,7 +173,8 @@ class CustomerPaymentsController < ApplicationController
    $lcBanco =@customerpayment.get_banco(@customerpayment.bank_acount.bank_id)  
    $lcCheque =@customerpayment.get_document(@customerpayment.document_id)+ "-"+@customerpayment.documento    
    
-   $lcFactory  = @customerpayment.get_customer_payment_value("factory").round(2).to_s  
+
+   $lcFactory  = sprintf("%.2f",@customerpayment.get_customer_payment_value("factory").round(2).to_s)  
 
       data0 = [[" "," "," "," ","TOTALES DEPOSITO => ",$lcDeposito ],
                [" "," "," "," ","COMISION FACTORY => ",$lcFactory],
