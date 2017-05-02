cd8d1e83aeccb217b448ce086fcc4bc85d5251d7
percywoeds@gmail.com <percywoeds@gmail.com>
2017-05-02 11:23:59 -0500

    abc

--- config/routes.rb
+++ config/routes.rb
@@ -334,6 +334,7 @@ end
   match 'facturas/ac_user/:company_id' => 'facturas#ac_user', via: [:get, :post]
   match 'facturas/ac_customers/:company_id' => 'facturas#ac_customers', via: [:get, :post]
   match 'facturas/ac_guias/:company_id' => 'facturas#ac_guias', via: [:get, :post]
+  
   match 'facturas/new/:company_id' => 'facturas#new', via: [:get, :post]
   match 'facturas/export/:company_id' => 'facturas#export', via: [:get, :post]
   match 'facturas/export2/:company_id' => 'facturas#export2', via: [:get, :post]
