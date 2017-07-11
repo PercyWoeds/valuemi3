079c5c0ca2ebb0c12944b501c91e8cf6c886e387
Percy Woeds <percywoeds@gmail.com>
2017-07-11 15:53:33 +0000

    a

--- app/models/lgv.rb
+++ app/models/lgv.rb
@@ -204,7 +204,7 @@ def get_total_inicial(items)
     return "#{self.code} "
   end
   def get_lgvs
-      @lgvs = LgvDetail.where(:lgv_id=> self.id)
+      @lgvs = LgvDetail.where(:id=> self.id)
   end
 
   def get_invoices
