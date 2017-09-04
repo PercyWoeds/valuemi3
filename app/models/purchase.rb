31147f55ba3480d836b5b7c07714f342bb7ff921
percy <percywoeds@gmail.com>
2017-09-04 11:21:48 -0500

    a

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -542,7 +542,7 @@ def get_tax3(items, supplier_id)
     
   end   
    def get_tipocambio(fecha1)
-     tipocambio = Tipocambio.find_by(["dia  >= ? and dia <= ? ", "#{$fecha1} 00:00:00","#{$fecha} 23:59:59" ])
+     tipocambio = Tipocambio.find_by(["dia  >= ? and dia <= ? ", "#{$fecha1} 00:00:00","#{$fecha1} 23:59:59" ])
      return tipocambio.compra 
   end 
   
