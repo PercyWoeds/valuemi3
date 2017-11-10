e9d9af3b8c3a312efd669012af90999ea6348b50
Percy Woeds <percywoeds@gmail.com>
2017-11-10 15:55:38 +0000

    a

--- app/models/company.rb
+++ app/models/company.rb
@@ -2688,7 +2688,8 @@ def get_ingresos_day4(fecha1,fecha2)
 
    @purchases = Purchase.find_by_sql(['Select purchases.*,purchase_details.quantity,
     purchase_details.price_without_tax as price,purchases.date1 as fecha, products.name as nameproducto,
-    products.code as codigo ,purchases.documento as code ,products.unidad,purchase_details.total,purchases.moneda_id
+    products.code as codigo ,purchases.documento as code ,products.unidad,purchase_details.total,
+    purchases.moneda_id,products.products_category_id 
     from purchase_details   
 INNER JOIN purchases ON purchase_details.purchase_id = purchases.id
 INNER JOIN products ON purchase_details.product_id = products.id
