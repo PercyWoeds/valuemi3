25216799828272188aa7df99d0e58e4c6c26e2e4
Percy Woeds <percywoeds@gmail.com>
2018-02-21 20:57:45 +0000

    a

--- app/models/viatico_detail.rb
+++ app/models/viatico_detail.rb
@@ -9,4 +9,12 @@ class ViaticoDetail < ActiveRecord::Base
     belongs_to :employee
     belongs_to :document 
     
+    
+    def get_detalle
+        ret =""
+        if self.tranportorder.code !="9999" 
+            ret = self.tranportorder.code << " - " << self.tranportorder.truck.placa<<" - " << self.tranportorder.get_placa(self.tranportorder.truck2_id)
+        end 
+        return ret 
+    end 
 end
