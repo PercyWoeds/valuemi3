8cab300d3ce21ad2653ca531681326f26370287b
Percy Woeds <percywoeds@gmail.com>
2018-04-03 22:14:17 +0000

    a

--- app/models/varillaje.rb
+++ app/models/varillaje.rb
@@ -185,7 +185,7 @@ class Varillaje < ActiveRecord::Base
          
         ret=0  
         for detalle in facturas
-            ret += detalle.importe.to_f
+            ret += detalle.implista
        end 
     end 
 
