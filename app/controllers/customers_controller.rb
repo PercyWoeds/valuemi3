1dc98c7465e70e221ea63ca020ad7aeb2cf8b801
Percy Woeds <percywoeds@gmail.com>
2018-04-20 17:01:12 +0000

    a

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -172,7 +172,7 @@ class CustomersController < ApplicationController
     end
 
     def customer_params
-      params.require(:customer).permit(:company_id,:email,:phone1,:phone2,:address1,:address2,:city,:state,:zip,:country,:comments,:account,:taxable,:name,:ruc)
+      params.require(:customer).permit(:company_id,:email,:phone1,:phone2,:address1,:address2,:city,:state,:zip,:country,:comments,:account,:taxable,:name,:ruc,:tipo)
     end
 
 end
