0dde9288c2305e6e9660f4fd43bba1634a5290b2
Percy Woeds <percywoeds@gmail.com>
2018-05-10 17:52:13 +0000

    a

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -623,81 +623,9 @@
     listItemsPurchase();
   }
 
-  // Shortcut to create new customer form
-  function createCustomerInvoice() {
-    var company_id = $("#invoice_company_id").val();
-    
-    $.get('/customers/new/' + company_id + '?ajax=1', {
-    },
-    function(data) {
-      displayRemote(data);
-      showRemote();
-      
-      $("#new_customer").bind("submit", function() {
-        event.preventDefault();
-        doCreateCustomerInvoice();
-      });
-    });
-  }
 
-  // Create new customer in the invoice via ajax
-  function doCreateCustomerInvoice() {
-    var company_id = $("#invoice_company_id").val();
-    var name = $("#customer_name").val();
-    var email = $("#customer_email").val();
-    var account = $("#customer_account").val();
-    var phone1 = $("#customer_phone1").val();
-    var phone2 = $("#customer_phone2").val();
-    var address1 = $("#customer_address1").val();
-    var address2 = $("#customer_address2").val();
-    var city = $("#customer_city").val();
-    var state = $("#customer_state").val();
-    var zip = $("#customer_zip").val();
-    var country = $("#customer_country").val();
-    var comments = $("#customer_comments").val();
-    
-    if($("#customer_taxable").attr("checked")) {
-      var taxable = 1;
-    } else {
-      var taxable = 0;
-    }
-    
-    if(name != "") {
-      $.post('/customers/create_ajax/' + company_id, {
-        name: name,
-        email: email,
-        account: account,
-        phone1: phone1,
-        phone2: phone2,
-        address1: address1,
-        address2: address2,
-        city: city,
-        state: state,
-        zip: zip,
-        country: country,
-        comments: comments,
-        taxable: taxable
-      },
-      function(data) {
-        if(data == "error_empty") {
-          alert("Please enter a customer name");
-        } else if(data == "error") {
-          alert("Something went wrong when saving the customer, please try again");
-        } else {
-          var data_arr = data.split("|BRK|");
-          
-          $("#ac_customer").val(data_arr[1]);
-          $("#ac_customer_id").val(data_arr[0]);
-          $("#selected_customer").html(data_arr[1]);
-          
-          hideRemote();
-          alert("The customer has been created");
-        }
-      });
-    } else {
-      alert("Please enter a customer name.");
-    }
-  }
+
+  
 
   // Add product kit to invoice
   function addKitToInvoice() {
@@ -1936,7 +1864,7 @@ function addItemToLgv2() {
 
   // Create new customer in the invoice via ajax
   function doCreateSupplier() {
-    var company_id = $("#factura_company_id").val();
+    var company_id = $("#factra_company_id").val();
     var ruc = $("#supplier_ruc").val();
     var name = $("#supplier_name").val();
     var email = $("#supplier_email").val();
@@ -2039,6 +1967,85 @@ function createMarca() {
       alert("Por favor ingrese una marca.");
     }
   }
+  
+  // Shortcut to create new customer form
+  function createCustomerInvoice() {
+    var company_id = $("#factura_company_id").val();
+    alert(company_id);
+    $.get('/customers/new/' + company_id + '?ajax=1', {
+    },
+    function(data) {
+      displayRemote(data);
+      showRemote();
+      
+      $("#new_customer").bind("submit", function() {
+        event.preventDefault();
+        doCreateCustomerInvoice();
+      });
+    });
+  }
+
+  // Create new customer in the invoice via ajax
+  function doCreateCustomerInvoice() {
+    var company_id = $("#factura_company_id").val();
+    var name = $("#customer_name").val();
+    var email = $("#customer_email").val();
+    var account = $("#customer_account").val();
+    var phone1 = $("#customer_phone1").val();
+    var phone2 = $("#customer_phone2").val();
+    var address1 = $("#customer_address1").val();
+    var address2 = $("#customer_address2").val();
+    var city = $("#customer_city").val();
+    var state = $("#customer_state").val();
+    var zip = $("#customer_zip").val();
+    var country = $("#customer_country").val();
+    var comments = $("#customer_comments").val();
+    
+    if($("#customer_taxable").attr("checked")) {
+      var taxable = 1;
+    } else {
+      var taxable = 0;
+    }
+    
+    if(name != "") {
+      $.post('/customers/create_ajax/' + company_id, {
+        name: name,
+        email: email,
+        account: account,
+        phone1: phone1,
+        phone2: phone2,
+        address1: address1,
+        address2: address2,
+        city: city,
+        state: state,
+        zip: zip,
+        country: country,
+        comments: comments,
+        taxable: taxable
+      },
+      function(data) {
+        if(data == "error_empty") {
+          alert("Please enter a customer name");
+        } else if(data == "error") {
+          alert("Something went wrong when saving the customer, please try again");
+        } else {
+          var data_arr = data.split("|BRK|");
+          
+          $("#ac_customer").val(data_arr[1]);
+          $("#ac_customer_id").val(data_arr[0]);
+          $("#selected_customer").html(data_arr[1]);
+          
+          hideRemote();
+          alert("The customer has been created");
+        }
+      });
+    } else {
+      alert("Please enter a customer name.");
+    }
+  }
+  
+  
+  
 
   // On ready
   $(document).ready(function() {
