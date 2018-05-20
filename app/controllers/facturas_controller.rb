b131a9da10f3ef35ede900d397d2257beaec3d0d
Percy Woeds <percywoeds@gmail.com>
2018-05-20 16:23:21 +0000

    a

--- app/controllers/facturas_controller.rb
+++ app/controllers/facturas_controller.rb
@@ -204,9 +204,10 @@ def reportes8
     @total_market = @company.get_ventas_market_total(@fecha1,@fecha2) #ventas market 
     
     @total_directa = @company.get_ventas_mayor(@fecha1,@fecha2,"3") #ventas market 
-    @total_adelantada = @company.get_ventas_mayor(@fecha1,@fecha2,"4") - @company.get_ventas_contometros_adelantad(@fecha1,@fecha2)
+    @total_adelantada_bruta = @company.get_ventas_mayor(@fecha1,@fecha2,"4") 
+    @total_adelantada = @total_adelantada_bruta - @company.get_ventas_contometros_adelantado(@fecha1,@fecha2)
     
-    @total_boletas= @company.get_ventas_contometros_efectivo(@fecha1,@fecha2) + @total_market + @total_directa + @total_adelantada  #ventas boletas reg ventas playa
+    @total_boletas= @company.get_ventas_contometros_efectivo(@fecha1,@fecha2) + @total_market + @total_directa + @total_adelantada_bruta   #ventas boletas reg ventas playa
     
     
     #@contado_rpt7 = @company.get_parte_6(@fecha1,@fecha2)
