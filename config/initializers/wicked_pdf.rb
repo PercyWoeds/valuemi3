c831786603a62b2083714e5f42d310b18077948f
Percy Woeds <percywoeds@gmail.com>
2018-05-25 22:11:38 +0000

    a

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -7,21 +7,8 @@
 # To learn more, check out the README:
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
-
-WickedPdf.config = {
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  # exe_path: '/usr/local/bin/wkhtmltopdf',
-  #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
-  # Layout file to be used for all PDFs
-  # (but can be overridden in `render :pdf` calls)
-  # layout: 'pdf.html',
- if Rails.env.production?
+if Rails.env.production?
   WickedPdf.config = {
     exe_path: Bundler.which('wkhtmltopdf')
   }
 end
-
-}
