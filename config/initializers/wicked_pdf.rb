7f4fcf041a7ad1f3676afc3a0f0999b12f2b09ad
Percy Woeds <percywoeds@gmail.com>
2018-05-28 16:03:47 +0000

    a

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -7,3 +7,8 @@
 # To learn more, check out the README:
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
+if Rails.env.production?
+  WickedPdf.config = {
+    exe_path: Bundler.which('wkhtmltopdf')
+  }
+end
