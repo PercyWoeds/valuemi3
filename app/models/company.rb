829949caf7f47cf29fbf594c9a1c14ec0c617261
Percy Woeds <percywoeds@gmail.com>
2018-06-04 21:33:41 +0000

    a

--- app/models/company.rb
+++ app/models/company.rb
@@ -22,6 +22,8 @@ class Company < ActiveRecord::Base
   has_many :inventories
   has_many :company_users
   has_many :ajusts 
+  
+  
 def get_facturas_day_cliente(fecha1,fecha2,cliente)
    
     @facturas = Factura.where(["total> 0  and  company_id = ? AND fecha >= ? and fecha<= ? and customer_id = ?", self.id, "#{fecha1} 00:00:00","#{fecha2} 23:59:59", cliente ]).order(:customer_id,:moneda_id,:fecha)
@@ -583,7 +585,7 @@ def get_guias_2(fecha1,fecha2)
 ## ESTADO DE CUENTA 
  def get_facturas_day(fecha1,fecha2,moneda)
    
-  a =  Sellvale.where(["fecha >= ? and fecha<= ? and td <> ? and importe2 IS NOT NULL ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59","N" ])
+  a =  Sellvale.where(["fecha >= ? and fecha<= ? and td <> ? and importe2 IS NULL ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59","N" ])
   
   for dato in a 
     
