dc10e456cb3d599fd4fd8ae4fa5a77a57d7d1583
Percy Woeds <percywoeds@gmail.com>
2018-08-13 21:09:14 +0000

    a

--- app/models/varillaje.rb
+++ app/models/varillaje.rb
@@ -339,7 +339,7 @@ def  get_inicial(fecha1,producto,producto2)
  
  def  get_ventas_contometros_tarjeta_factura(fecha) 
 
-     facturas = Sellvale.where(["fecha >= ? and fecha <= ?  and fpago <> ?  and td <> ?  and tipo = ?" , "#{fecha} 00:00:00","#{fecha} 23:59:59", "1" ,"F","1"])
+     facturas = Sellvale.where(["fecha >= ? and fecha <= ?  and fpago <> ?  and td = ?  and tipo = ?" , "#{fecha} 00:00:00","#{fecha} 23:59:59", "1" ,"F","1"])
      
      if facturas
          
