e6a1150a2ce41cbfdcf9f8aeb1dde8f9eb13e55f
Percy Woeds <percywoeds@gmail.com>
2018-10-10 20:31:09 +0000

    a

--- app/models/company.rb
+++ app/models/company.rb
@@ -715,7 +715,7 @@
           #market
            @boletas = Market.select("fecha,td,serie,ruc,MIN(numero) as minimo, MAX(numero) as maximo,sum(importe) as total").where(["fecha >= ? and fecha<= ? and td= ? ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59","B" ]).group(:fecha,:td,:serie,:ruc)
         # @boletas = Sellvale.select("fecha,td,cod_prod,ruc,MIN(numero) as minimo, MAX(numero) as maximo,sum(importe2) as total").where(["fecha >= ? and fecha<= ? and td<> ? ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59","N" ]).group(:fecha,:td,:serie,:numero,:cod_prod,:ruc)
-          TmpFactura.delete_all
+    
           
           for boleta in @boletas  
           
