6d2f47aabacea81e32f33eef64bbb303dbda58f2
Percy Woeds <percywoeds@gmail.com>
2018-11-07 21:05:11 +0000

    a

--- app/models/note_concept.rb
+++ app/models/note_concept.rb
@@ -1,6 +1,5 @@
 class NoteConcept < ActiveRecord::Base
-    
-    validates_presence_of  :descrip,:document_id ,:td
+     validates_presence_of  :descrip,:document_id ,:td
 
   belongs_to :document
 end
---
+++ db/migrate/20181107210428_add_td_to_note_concepts.rb
@@ -0,0 +1,6 @@
+class AddTdToNoteConcepts < ActiveRecord::Migration
+  def change
+    add_column :note_concepts, :td, :string
+    add_column :note_concepts, :document_id, :integer
+  end
+end
