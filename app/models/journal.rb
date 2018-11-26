6cc5cd888164107afdcba31aaa4694baa1bc6864
Percy Woeds <percywoeds@gmail.com>
2018-11-26 22:20:29 +0000

    a

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,10 +2,8 @@ class Journal < ActiveRecord::Base
     
     
      def self.to_csv
-    attributes = %w{id , journal, nid_journal, ffecha_journal, nid_surtidor,
-    nlado_surtidor, nid_producto, nposicion_manguera, dprecio_journal, dvolumen_journal,
-    dmonto_journal, dcontometrogalon_journal, dcontometromoneda_journal, ffechacontable_journal, 
-    turno, nturno_journal, nestadodesp, nestadocont: string, ntransactionid_journal,
+    attributes = %w{id  journal nid_journal ffecha_journal nid_surtidor nlado_surtidor  nid_producto nposicion_manguera dprecio_journal dvolumen_journal 
+    dmonto_journal dcontometrogalon_journal dcontometromoneda_journal ffechacontable_journal turno  nturno_journal nestadodesp nestadocont ntransactionid_journal
     ntranfinished_journal}
 
     CSV.generate(headers: true) do |csv|
