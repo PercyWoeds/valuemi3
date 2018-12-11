44f8a474c1c424e8de5bdcdfa1c2bb9e76cd780e
Percy Woeds <percywoeds@gmail.com>
2018-12-11 20:38:26 +0000

    a

--- app/models/company.rb
+++ app/models/company.rb
@@ -3666,7 +3666,7 @@
        # Ventas grifero 
        def  get_parte_detalle_grifero(fecha1,fecha2,grifero,turno ) 
          
-           @contado = Sellvale.where(["fecha >= ? and fecha <= ?  and  grifero = ? and turno = ? ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59" ,"2" ]).order(:cod_prod,:fecha)
+           @contado = Sellvale.where(["fecha >= ? and fecha <= ?  and  grifero = ? and turno = ? ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59" ,grifero,turno ]).order(:cod_prod,:fecha)
          
           return @contado
        end 
