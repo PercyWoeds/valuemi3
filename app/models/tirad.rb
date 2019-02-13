f490d92c6c867a6c1284e553c9d145585bbafec2
Percy Woeds <percywoeds@gmail.com>
2019-02-13 14:11:44 +0000

    a

--- app/models/tirad.rb
+++ app/models/tirad.rb
@@ -5,7 +5,19 @@ class Tirad < ActiveRecord::Base
     
     def self.import(file)
           CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
+              
+          a = Employee.find_by(cod_emp: row['cod_emp'].rjust(4, '0')   )
+          
+          if a == nil
+                row['employee_id'] = 10
+          else
+                row['employee_id'] = a.employee_id
+                
+          end 
+              
+          
           Tirad.create! row.to_hash 
+          
         end
     end     
 
