a0a63382abcdd1c76e457273b4064f09a36fe824
Percy <percywoeds@gmail.com>
2020-12-04 12:13:49 -0500

    a

--- config/initializers/nubefact.rb
+++ config/initializers/nubefact.rb
@@ -1,5 +1,5 @@
 require "./lib/nubefact/nube_fact"
 
-    NubeFact.url_token = 'f4782edc-ffdc-4c26-87f5-f6716f79ce44'
-    NubeFact.api_token = 'a9de24db083d45a2a00d8a5fc2da2d5776bae35da9fd4ffb82fd4b339e72b896'
+    NubeFact.url_token = '6bc7a205-1093-423c-90c5-b5eb9b2ed27a'
+    NubeFact.api_token = '400eccc816634b9293f3fe4b3d05efe408c64642440344e3aefa8cefb86edd6d'
 
