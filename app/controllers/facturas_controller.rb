f55f1a51cfbdbb7eda84e33f79542bcd92c1238f
Percy Woeds <percywoeds@gmail.com>
2021-12-17 12:22:24 -0500

    ddd

--- app/controllers/facturas_controller.rb
+++ app/controllers/facturas_controller.rb
@@ -1657,7 +1657,7 @@ def reportes31
   def do_process2
     @invoice = Factura.find(params[:id])
     @invoice[:processed] = "1"
-    @invoice.process2
+   # @invoice.process2
     
     flash[:notice] = "The invoice order has been processed."
     redirect_to @invoice
