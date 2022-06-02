bf6dc0ad422a350d962eb9e47e9d795f6ead5307
PercyWoeds <percywoeds@gmail.com>
2022-06-02 09:18:55 -0500

    inicio

--- config/config.rb
+++ config/config.rb
@@ -3,31 +3,30 @@ SUNAT.configure do |config|
   config.credentials do |c|
 
 
-    c.ruc       = "20514739065"
+    c.ruc       = "20555691263"
     c.username  = "GEYE2018"
-    c.password  = "20514739065"
+    c.password  = "20555691263"
    end
 
   config.signature do |s|
-    s.party_id    = "20514739065"
-    s.party_name  = "GRUPO E & E S.A.C."
+    s.party_id    = "20555691263"
+    s.party_name  = " INVERSIONES VALUEMI S.A.C."
     s.cert_file   = File.join(Dir.pwd, './app/keys', 'certificategee.crt')
     s.pk_file     = File.join(Dir.pwd, './app/keys', 'CERTIFICADOGEE.key') 
   end
 
   config.supplier do |s|
-    s.legal_name = "GRUPO E & E S.A.C."
+    s.legal_name = " INVERSIONES VALUEMI S.A.C."
     s.name       = ""
-    s.ruc        = "20514739065"
+    s.ruc        = "20555691263"
     s.address_id = "150132"
-    s.street     = "AV. TUPAC AMARU KM. 22.5 LOTE. 7 URB. PUNCHAUCA
-    (ALT.PARADERO LAS AMERICAS LT8)"
+    s.street     = "CAM.SECTOR CRUZ DEL NORTE I ZONA BAJA MZA. C LOTE. 5 A.H. PROYECTO INTEGRAL ALIANZA INDUSTRIAL DE LAS LOMAS (ANTES PARADERO HUARANGO 4KMOVALO ZAPALLA)"
     s.district   = "CARABAYLLO - LIMA - LIMA   "
     #s.city       = "LOCAL: AV. CERRO DE PASCO NRO. 253 (A DOS CDAS DEL MUNICIPIO)
     #PASCO - PASCO - PAUCARTAMBO"
     s.city       = ""
     s.country    = "PE"
-    s.logo_path  = "#{Dir.pwd}/app/assets/images/logo.png"
+    s.logo_path  = "#{Dir.pwd}/app/assets/images/valuemi_logo.jpg"
 
                           
                           
