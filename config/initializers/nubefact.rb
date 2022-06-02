3b96a3dcbcb2718af4e57934ccb5830d2deb5a3d
PercyWoeds <percywoeds@gmail.com>
2022-06-02 09:43:57 -0500

    nubefact

--- config/initializers/nubefact.rb
+++ config/initializers/nubefact.rb
@@ -1,9 +1,11 @@
 require "./lib/nubefact/nube_fact"
 
-    NubeFact.url_token = '6bc7a205-1093-423c-90c5-b5eb9b2ed27a'
-    NubeFact.api_token = '400eccc816634b9293f3fe4b3d05efe408c64642440344e3aefa8cefb86edd6d'
+    NubeFact.url_token = '693078e0-353f-46a5-a1b0-e8451ad5d0a3'
+    NubeFact.api_token = 'e2d499ff5d8949e99b09dc998a57fc43ac6e92d1a9f7430d90a9cf0f76bf7cb8'
 
  #   https://api.nubefact.com/api/v1/f4782edc-ffdc-4c26-87f5-f6716f79ce44
  #   f3e98eb084d04779ad506740743a2050c7ac21c5e609477fb2162a2d396c8ccb
  #   NubeFact.url_token = 'f4782edc-ffdc-4c26-87f5-f6716f79ce44'
  #   NubeFact.api_token = 'f3e98eb084d04779ad506740743a2050c7ac21c5e609477fb2162a2d396c8ccb'
+
+
