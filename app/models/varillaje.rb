23c3a2e6f09e310c5f1c93a850020f357f38f351
PercyWoeds <percywoeds@gmail.com>
2022-06-21 21:08:07 -0500

    a

--- app/models/varillaje.rb
+++ app/models/varillaje.rb
@@ -503,7 +503,7 @@ def  get_inicial(fecha1,producto,producto2)
      WHERE products.products_category_id = 1 
      and sellvales.fecha >= ? 
      and sellvales.fecha <= ? 
-     and cod_tar = ? || cod_tar = ? 
+     and cod_tar = ? or cod_tar = ? 
      ORDER BY sellvales.fecha', "#{fecha} 00:00:00","#{fecha} 23:59:59","05","07" ])
      
      
